18a38a73f0452da91a7de7add53bfcd4f93efe43
Eliton lima <elitonlima2@hotmail.com>
2022-08-16 10:09:06 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.SellerDaoJDBC;
+
 public class DaoFactory {
+	// metodo que retorna o tipo de interface
+	public static SellerDao createSellerDao( ) {
+		// internamente vai instância uma implementação
+		return new SellerDaoJDBC(DB.getConnection());
+		
+	}
 
 }
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,18 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
+	
+	// vai inserir no banco de dados esse objeto que for enviado como parâmetro de entrada.
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	// operação responsável por consultar no banco um objeto com esse ID se existir vai retorna senão retorna NULL
+	Department findById(Integer id);
+	// pra retornar todos os departamentos.
+	List<Department> findAll();
 
 }
